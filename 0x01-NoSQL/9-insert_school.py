d6727d95a5f94dd2afbf7df2623384d4fca25dc7
codeme254 <otwomadennis365@gmail.com>
2023-05-09 11:54:54 +0300

    insert using python

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+inserts a new document in a collection based on kwargs
+then returns the new _id
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    inserts a new document in a collection based on kwargs
+    then returns the new _id
+    """
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
